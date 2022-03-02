a8e2af9a20f51fbc5878cdf741fbe3dd54b2dc85
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-02 20:30:25 +0530

    Calculte Daily Employee Wage

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EmployeeWage
+{
+    public class Employee
+    {
+        public static void Main(string[] args)
+        {
+            int EmployeePresent = 1;
+            int EmployeeWagePerHour = 20;
+            int WorkingHour = 8;
+            int Wage = EmployeeWagePerHour * WorkingHour;
+
+            Random random = new Random();
+            int EmployeeCheck = random.Next(2);
+
+            {
+                if (EmployeeCheck == EmployeePresent)
+
+                {
+                    Console.WriteLine("Employee Wage is:" + Wage);
+                }
+
+                else
+
+                {
+                    Console.WriteLine("Employee Wage is Zero");
+                }
+            }
+        }
+    }
+}
