99ef18c2630dfee0052dd50bf67d1406c1e94333
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-02 20:25:23 +0530

    Welcome Messege

---
+++ Greeting/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace greeting
+{
+    public class greeting
+    {
+        public static void Main(string[] agrs)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computaion Program");
+        }
+    }
+}
