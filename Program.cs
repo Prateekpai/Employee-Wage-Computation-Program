55635f28c458192d01e09c7cd04742e5bbfa3778
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-02 21:02:53 +0530

    Calculating Wages till a condition of total working hours or day is reached for a Month

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EmployeeWage
+{
+    public class Employee
+    {
+        public static void Main(string[] args)
+        {
+            int EmployeePresent = 1;
+            int EmployeeWagePerHour = 20;
+
+            int EmpHour = 0;
+            int EmpWage = 0;
+
+            int totalEmpHour = 0;
+            int totalEmpWorkingDays = 0;
+
+            int TotalWorkingHour = 100;
+            int TotalWorkingDays = 20;
+
+            while (totalEmpHour < TotalWorkingHour || totalEmpWorkingDays < TotalWorkingDays)
+            {
+                totalEmpWorkingDays++;
+                Random random = new Random();
+                int EmployeeCheck = random.Next(2);
+
+                if (EmployeeCheck == EmployeePresent)
+
+                {
+                    Console.WriteLine("Employee is Present");
+                    EmpHour = 8;
+                }
+
+                else
+
+                {
+                    Console.WriteLine("Employee is Absent");
+                    EmpHour = 0;
+                }
+                totalEmpHour = totalEmpHour + EmpHour;
+                {
+                    Console.WriteLine("Total Employee Working Hour is :" + totalEmpHour);
+                }
+            }
+        }
+    }
+}
