d04be3c8aea9ddc4497474529cee58ecad4ab742
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-07 19:45:37 +0530

    Length of Line

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+		int x1, x2, y1, y2;
+
+		Console.WriteLine("Enter X1 Value:");
+		x1 = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Enter X2 Value:");
+		y1 = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Enter Y1 Value:");
+		x2 = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Enter Y2 Value:");
+		y2 = Convert.ToInt32(Console.ReadLine());
+
+		double Z = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		Console.WriteLine("Length of Line is : {0}", Z);
+	}
+}
+
